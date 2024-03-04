7a8efcd7cb06d6628a2b799c992084bda202c83c
Rocco Debellis <rocdebellis@gmail.com>
2024-03-04 22:47:54 +0100

    Add script to install project dependencies
    Added a new script named 'install_dependencies.zsh' to automate the installation of project dependencies. The script checks if 'make' is installed and then runs the 'make system_dependencies' command to install the dependencies. Additionally, colored messages have been added for improved readability.

---
+++ install_dependencies.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RESET='\033[0m' # Reset color
+
+# Script to run 'make system_dependencies'
+
+# Check if make is installed
+if ! command -v make &> /dev/null
+then
+    echo "${RED}Make not found. Make sure it is installed.${RESET}"
+    exit 1
+fi
+
+# Run the 'make system_dependencies' command
+echo "⚙️  ${YELLOW}Running 'make system_dependencies'...${RESET}"
+make system_dependencies && echo "${GREEN}Dependencies installed successfully.${RESET}"
